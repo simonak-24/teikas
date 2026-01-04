5499a03f553d88d5d2d3e142474c497a8c8656f9
simonak-24 <simonchiks@gmail.com>
2026-01-04 12:57:40 +0200

    bug fix: redirect to login page now working correctly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\LanguageMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append:[
             App\Http\Middleware\LanguageMiddleware::class,
         ]);
+        
+        $middleware->redirectGuestsTo(fn (Request $request) => route('login', ['lang' => Session::get('locale') ?? 'lv']));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
