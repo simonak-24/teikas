381921e3e8a8a1e5966c2c5f4c28b95c14b25b4e
simonak-24 <simonchiks@gmail.com>
2026-01-19 13:37:12 +0200

    pagination improved

--- resources/views/navigation/pagination.blade.php
+++ resources/views/navigation/pagination.blade.php
@@ -1,27 +1,31 @@
 @if($paginator->lastPage() > 1)      
 <div id="pagination-links">
-    <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url(1) }}"> << </a></div>
-    <div class="pagination-button"><a href="{{ $paginator->withQueryString()->previousPageUrl() }}"> < </a></div>
+    @if($paginator->currentPage() != 1)
+        <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url(1) }}"> << </a></div>
+        <div class="pagination-button"><a href="{{ $paginator->withQueryString()->previousPageUrl() }}"> < </a></div>
+    @endif
     @if($paginator->lastPage() <= 9)
         @for ($i = 1; $i <= $paginator->lastPage(); $i++)
-            <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a></div>
+            <div class="pagination-button {{ $paginator->currentPage() == $i ? 'pagination-selected' : '' }}"> @if($paginator->currentPage() != $i) <a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a> @else <span> {{ $i }} </span> @endif </div>
         @endfor
     @else
         @if($paginator->currentPage() <= 5)
             @for ($i = 1; $i <= 9; $i++)
-                <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a></div>
+                <div class="pagination-button {{ $paginator->currentPage() == $i ? 'pagination-selected' : '' }}"> @if($paginator->currentPage() != $i) <a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a> @else <span> {{ $i }} </span> @endif </div>
             @endfor
         @elseif($paginator->currentPage() + 5 > $paginator->lastPage())
             @for ($i = $paginator->lastPage() - 9; $i <= $paginator->lastPage(); $i++)
-                <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a></div>
+                <div class="pagination-button {{ $paginator->currentPage() == $i ? 'pagination-selected' : '' }}"> @if($paginator->currentPage() != $i) <a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a> @else <span> {{ $i }} </span> @endif </div>
             @endfor
         @else
             @for ($i = $paginator->currentPage() - 4; $i <= $paginator->currentPage() + 4; $i++)
-                <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a></div>
+                <div class="pagination-button {{ $paginator->currentPage() == $i ? 'pagination-selected' : '' }}"> @if($paginator->currentPage() != $i) <a href="{{ $paginator->withQueryString()->url($i) }}"> {{ $i }} </a> @else <span> {{ $i }} </span> @endif </div>
             @endfor
         @endif
     @endif
-    <div class="pagination-button"><a href="{{ $paginator->withQueryString()->nextPageUrl() }}"> > </a></div>
-    <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($paginator->lastPage()) }}"> >> </a></div>
+    @if($paginator->currentPage() != $paginator->lastPage())
+        <div class="pagination-button"><a href="{{ $paginator->withQueryString()->nextPageUrl() }}"> > </a></div>
+        <div class="pagination-button"><a href="{{ $paginator->withQueryString()->url($paginator->lastPage()) }}"> >> </a></div>
+    @endif
 </div>
 @endif
